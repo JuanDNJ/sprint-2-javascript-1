08df2238379ecbdac6491e392d568ef5a1de30fe
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-02-22 20:41:24 +0100

    Exercici 1.2: Operador ternari, Exercici 2

--- index.js
+++ index.js
@@ -76,6 +76,8 @@ timeOut3000();
 
 // Exercici 1.2: Operador ternari
 
+// NIVELL 1
+
 /** Exercici 1
  * Operador ternari bàsic: 
  * Escriu una funció potConduir que accepti l'edat com a paràmetre i utilitzi l'operador
@@ -88,4 +90,19 @@ const potCounduir = (edat) => {
 let anys = 18;
 console.log(`Tinc ${anys} anys puc conduir: `, potCounduir(anys));
 anys = 17;
-console.log(`Tinc ${anys} anys puc conduir: `, potCounduir(anys))
+console.log(`Tinc ${anys} anys puc conduir: `, potCounduir(anys));
+
+/** Exercici 2
+ * Ús amb operadors de comparació: 
+ * Escriu una expressió que utilitzi l'operador ternari per determinar quin dels dos nombres
+ * donats (num1 i num2) és més gran. Si num1 és més gran, retorna 'num1 és més gran'. 
+ * Si no, retorna 'num2 és més gran'.
+ */
+
+const num1 = 45;
+const num2 = 56;
+const quinNombresEsMesGran = (num1, num2) => {
+    return num1 > num2 ? `Num 1 ${num1} és més gran` : `Num 2 ${num2} és més gran`;
+}
+console.log(quinNombresEsMesGran(45, 56))
+
