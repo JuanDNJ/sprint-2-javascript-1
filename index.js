c4c4ca3e51c02814160f0dc8ab7eb69d66588751
Juan Antonio <132897973+JuanDNJ@users.noreply.github.com>
2024-02-22 19:55:45 +0100

    Exercici 4

--- index.js
+++ index.js
@@ -8,40 +8,58 @@
     Per exemple: function add(a, b) {return a + b;}.
 */
 
-const add  = (a, b) => {
-    return a + b;
-}
+const add = (a, b) => {
+  return a + b;
+};
 
 console.log("Suma de dos números: ", add(23, 20));
 
 /** Exercici 2
- * Funció de fletxa sense paràmetres: Crea una funció de fletxa anomenada 
- * randomNumber que no necessiti paràmetres i que retorni un número aleatori 
+ * Funció de fletxa sense paràmetres: Crea una funció de fletxa anomenada
+ * randomNumber que no necessiti paràmetres i que retorni un número aleatori
  * entre 0 i 100.
  */
 
 const randomNumber = () => {
-    return Math.floor(Math.random() * 100);
-}
-console.log("Número aleatorio del 0 al 100: ",randomNumber());
+  return Math.floor(Math.random() * 100);
+};
+console.log("Número aleatorio del 0 al 100: ", randomNumber());
 
 /** Exercici 3
  * Ús de 'this' en les funcions de fletxa: Crea una classe person que tingui
  * una propietat name i una funció greet que utilitzi una funció de fletxa.
- * La funció ha d'imprimir una salutació que inclogui el nom de la persona. 
+ * La funció ha d'imprimir una salutació que inclogui el nom de la persona.
  * Per exemple: console.log(Hola, ${this.name});.
-*/
+ */
 class Person {
-    name = "Juan Valdivia";
+  name = "Juan Valdivia";
 
-    greet() {
-        const printName = () => console.log(`Buenos días ${this.name}`);
-        printName();
-    }
+  greet() {
+    const printName = () => console.log(`Buenos días ${this.name}`);
+    printName();
+  }
 }
 
 const person = new Person();
-person.greet()
+person.greet();
 
+// ----- NIVELL 2 -----
+
+/** Exercici 4
+ * Funció de fletxa dins d'un loop: Crea una funció anomenada printNumbers
+ * que accepti un array de números i utilitzi un loop for per imprimir cada
+ * número a la consola utilitzant una funció de fletxa.
+ */
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const printNumbers = (listNumbers) => {
+    listNumbers.forEach(element => {
+        const printNumber = () => console.log("Número: ", element)
+        printNumber();
+    });
+}
+
+printNumbers(numbers)
 
 
